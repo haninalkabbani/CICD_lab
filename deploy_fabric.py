a58eb587825e9e55338511c6b3ac54bb1ba507b7
haninalkabbani <hanin.alkabbani@gmail.com>
2026-04-20 12:08:09 -0400

    Create deploy_fabric.py

---
+++ deploy_fabric.py
@@ -0,0 +1,22 @@
+from azure.identity import ClientSecretCredential
+from fabric_cicd import FabricWorkspace, publish_all_items
+
+tenant_id = "${TENANT_ID}"
+client_id = "${CLIENT_ID}"
+client_secret = "${CLIENT_SECRET}"
+
+credential = ClientSecretCredential(
+tenant_id=tenant_id,
+client_id=client_id,
+client_secret=client_secret,
+)
+
+target_workspace = FabricWorkspace(
+workspace_name="Fabric_lab_1_Prd",
+repository_directory=".",
+item_type_in_scope=["Notebook", "DataPipeline", "Lakehouse", "Report", "SemanticModel"],
+environment="PROD",
+token_credential=credential,
+)
+
+publish_all_items(target_workspace)
